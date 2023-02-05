465fa0375d816b045819f9f636659ca00268adc4
Replit user <>
2023-02-05 21:39:12 +0000

    Fix logo div position on onload, and add notes to the javascript

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
+
+//vairables
+
 const submitBtn = document.getElementById("submitButton"), passwordElmnt = document.getElementById("password"), passwordConfirmElmnt = document.getElementById("confirm_password");
 
 submitBtn.onclick = function() {
-  if (passwordElmnt.value != passwordConfirmElmnt.value) {
-    const passwordErrorDiv = document.createElement("div");
-    document.getElementById("confirmPasswordInputContainer").appendChild(passwordErrorDiv);
-    passwordConfirmElmnt.setCustomValidity("Passwords Don't Match");
+  if (passwordElmnt.value != passwordConfirmElmnt.value) { //checks if the passwords are the same in both password inputs; 
+    //if not same, will give an error message that they don't match 
+    passwordConfirmElmnt.setCustomValidity("Passwords Don't Match"); //shows error that passwords don't match
   }
   else {
-    passwordConfirmElmnt.setCustomValidity("");
+    passwordConfirmElmnt.setCustomValidity(""); //no error message
   }
 }
 
-function checkWidth() {
+//functions
+
+function checkWidth() { //checks width of the page, then sets the order of the page divs
   let width = $("body").width();
-  if (width <= 980) {
+  if (width <= 980) { //width less than or equal to 980 pixels will center the content
     document.body.style.display = "block";
     document.getElementById("leftContainer").style.height = "400px";
     document.getElementById("mainForm").style.alignItems = "center";
     document.getElementById("myForm").style.justifyContent = "center"
     document.getElementById("filler1").style.alignItems = "center";
     document.getElementById("filler2ContentContainer").className = "filler2ContentContainerCenter";
+    logoDiv.style.top = "100px";
   }
-  else {
+  else { //width greater than 980 pixels will set the page flow to the original positions and order
     document.body.style.display = "flex";
     document.getElementById("leftContainer").style.height = "100%";
     document.getElementById("filler1").style.alignItems = "";
     document.getElementById("mainForm").style.alignItems = "";
     document.getElementById("myForm").style.justifyContent = ""
     document.getElementById("filler2ContentContainer").className = "";
+
+    const filler1Height = $("#filler1").height(), logoDiv = document.getElementById("logoDiv"); //checks height of text area using jquery and assigns variables
+    let height = filler1Height - logoDiv.offsetHeight;
+
+    logoDiv.style.top = height + "px";
+    console.log(logoDiv.style.top); //for troubleshooting
   }
 }
 
-function setLogoPosition() {
-  const filler1Height = $("#filler1").height(), logoDiv = document.getElementById("logoDiv");
-
-  let height = filler1Height - logoDiv.offsetHeight;
-
-  logoDiv.style.top = height + "px";
-  console.log(logoDiv.style.top);
-}
+//onload and listeners
 
 window.onload = () => {
-  setLogoPosition();
   checkWidth();
 }
 
 window.addEventListener("resize", checkWidth);
-window.addEventListener("resize", setLogoPosition);
+
 
